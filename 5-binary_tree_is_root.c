15ff35beaf53d72c634e8b20e87a5a581e886c30
Mickey-34 <abbeyella54@gmail.com>
2023-10-06 13:41:10 -0700

    Binary Task 5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - check node root or not
+ * @node: node to check
+ * This is Mimi's Binary project Alx
+ *
+ * Return: 1 if node if root 0 if not
+ *
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	else if (node->parent == NULL)
+		return (1);
+	return (0);
+}
