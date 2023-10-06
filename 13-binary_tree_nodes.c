50d60ce00dc7d1a2f2b18dc6222762b4c7d3a8ba
Mickey-34 <abbeyella54@gmail.com>
2023-10-06 14:35:59 -0700

    Binary Task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,7 +11,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	else if (node->l == NULL && node->r == NULL)
+	else if (node->left == NULL && node->right == NULL)
 		return (1);
 	return (0);
 }
@@ -26,13 +26,13 @@ int binary_tree_is_leaf(const binary_tree_t *node)
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t hl, hr;
+	size_t h_left, h_right;
 
 	if (tree == NULL || binary_tree_is_leaf(tree) == 1)
 		return (0);
 
-	hl = binary_tree_nodes(tree->l);
-	hr = binary_tree_nodes(tree->r);
+	h_left = binary_tree_nodes(tree->left);
+	h_right = binary_tree_nodes(tree->right);
 
-	return (hr + hl + 1);
+	return (h_right + h_left + 1);
 }
