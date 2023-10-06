50f5a5af070caef0aed219768bf9ca5c4084ce86
Mickey-34 <abbeyella54@gmail.com>
2023-10-06 14:26:07 -0700

    Binary Task 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - check node leaf or not
+ * @node: node to check
+ * This is Mimi's alx Binary Project
+ *
+ * Return: 1 if node if leaf 0 if not
+ *
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	else if (node->l == NULL && node->r == NULL)
+		return (1);
+	return (0);
+}
+/**
+ * binary_tree_leaves - measures the leaves of the tree
+ * @tree:pointer to the root node of the tree
+ * This is Mimi's Binary alx project
+ *
+ * Return: 0 if tree is NULL else the size leaves
+ *
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t hl, hr;
+
+	if (tree == NULL)
+		return (0);
+	if (binary_tree_is_leaf(tree))
+		return (1);
+	hl = binary_tree_leaves(tree->l);
+	hr = binary_tree_leaves(tree->r);
+
+	return (hr + hl);
+
+}
